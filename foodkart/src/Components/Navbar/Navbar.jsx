68989208c22ee6282e91d703cc9a4cc48fe24f69
Rishav Kumar <rishavraj26062001@gamil.com>
2023-08-21 19:36:17 +0530

    no msg

--- foodkart/src/Components/Navbar/Navbar.jsx
+++ foodkart/src/Components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import './Navbar.css'
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { UserContext } from '../../contexts/UserContext';
 
 function NavBar() {
